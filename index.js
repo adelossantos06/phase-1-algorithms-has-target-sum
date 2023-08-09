ea2439b9c83a828830cb6c60295f91c2b17ff129
Alyssa De Los Santos <adelossantos6@gmail.com>
2023-08-09 17:56:27 -0500

    completes target sum lab

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+
+  for (let i = 0; i < array.length; i++) {
+    const secondNum = target - array[i];
+    for (let b = i + 1; b < array.length; b++) {
+      if (array[b] === secondNum) return true
+    }
+
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
   Add your pseudocode here
+  iterate thru each num in the array
+  for current num identify second num that equals target num 
+  if so, return true 
+  if not, return false 
 */
 
 /*
   Add written explanation of your solution here
+  the function contains a for loop 
+  the for loop iterates thru each number looking for a second number to complete the taget number
+  if a second number is found then the function returns true
+  if a second number cannot be found then it will return false 
 */
 
 // You can run `node index.js` to view these console logs
